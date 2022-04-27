c4e6fb359ca12fc93c2e31b155fdbc187fbc9cc9
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-04-27 19:12:28 +0530

    Python module to pull metaweather data from api for each cities in USA

--- api_pull_metaweather_data/pull_data_each_city_from_api.py
+++ api_pull_metaweather_data/pull_data_each_city_from_api.py
@@ -1,34 +1,33 @@
-""" This module is to pull data from api for each cities in US"""
+""" This module is to pull data from api for each cities in USA"""
+import sys
 import requests
-import configparser
-from datetime import datetime,timedelta
 
 
-config =configparser.ConfigParser()
-config.read('MSG-LAMP Practice')
-
-class PullDataFromApi:
+class PullDataFromMetaweatherApi:
     """This class pulls data from an api as per their woeid-where on earth id"""
-    def __init__(self):
-        """This is the init method of the PullData class"""
-        # self.city_woied = config['woeid']['Albuquerque']
-        self.date = datetime.now()
-        
-    def get_weather_data_cities(self):
-        """This method used to get the details using the woeid of city from api"""
-        search_date = self.date.strftime('%Y-%m-%d')
-       
-        # for key,value in config['woeid']['cities_woeid'].items():
-        for key,value in config.items('woeid'):
-            # print(value)
-            # print(key)
-            response = requests.get('https://www.metaweather.com/api/'+value+'/'+search_date+'/').json()
-            print(response)
-            
-# def main():
-#     """This is the main method of the class """
-#     pull=PullDataFromApi()
-#     pull.get_weather_data_cities()
 
-# if __name__=='__main__':
-#     main()
+    def __init__(self, logger):
+        """This is the init method of the PullData class"""
+        self.logger = logger
+
+    def get_weather_data_cities_using_woeid_from_api(self, woeid, search_date):
+        """This method used to get the details using the woeid of city from
+        api "/api/location/(woeid)/(date)/" for the provided date.
+        If any problem in api terminate the execution"""
+
+        try:
+            response = requests.get(
+                "https://www.metaweather.com/api/location/{woeid}/{date}/".format(
+                    woeid=woeid, date=search_date
+                )
+            ).json()
+            self.logger.info("The Metaweather information of each city are got from the api")
+            # print(response)
+            return response
+
+        except Exception as err:
+            print("Cannot able to fetch the MetaWeather data of woeid due to Problems in api", err)
+            self.logger.info(
+                "Cannot able to fetch the MetaWeather data of woeid due to Problems in api"
+            )
+            sys.exit()
